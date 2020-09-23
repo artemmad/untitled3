3c85ec1db655dfeef861cc1453813c19f7664a27
Artem <dwadawdawd>
2020-09-23 17:33:34 +0300

    Start of project

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Scanner in =  new Scanner(System.in);
+        int a = in.nextInt();
+        int b = in.nextInt();
+        System.out.println(a+b);
+
+    }
+}
